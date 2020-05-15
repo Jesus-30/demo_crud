03bef7457dbc87df45aa136fc19128c901dce0f7
jorge Lopez <jr1552059@gmail.com>
2020-05-15 17:47:29 -0600

    agregue el metodo delete

--- src/recursohumano/Delete.java
+++ src/recursohumano/Delete.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package recursohumano;
 
-/**
- *
- * @author Estefany
- */
+import java.sql.SQLException;    // Libreria importada
+import java.util.Scanner;        // Libreria importada
+
 public class Delete {
-    
+    Delete() throws SQLException {
+        Scanner leer = new Scanner(System.in);
+        ConexionCRUD utilerias = new ConexionCRUD();
+        System.out.println(" << ELIMINAR REGISTROS >> ");
+        
+        System.out.println("Ingresa el id del registro: ");
+        String idContactoEliminar = leer.next();
+        // Registro de datos para eliminar 
+        String tabla = "tb_contacto";
+        String campos = "*";
+        String condicion = "id_contacto = " + idContactoEliminar;
+        utilerias.desplegarRegistros(tabla, campos, condicion);
+        
+        System.out.println("Presionar << Y >> para confirmar: ");
+        String confirmarBorrar = leer.next();
+        
+        if ("Y".equals(confirmarBorrar)){
+            /*Se le deja vacio para el método actualizarEliminarRegistros
+            envie solamente los parámetros de la tabla y condición y poder eliminar*/
+            String valoresCamposNuevos = "";
+            
+            utilerias.actualizarEliminarRegistros(tabla, valoresCamposNuevos, condicion);
+            System.out.println("Registro borrado satisfactoriamente!");
+        }
+        MenuPrincipal.desplegarMenu();
+    }
 }
